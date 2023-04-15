70311aa7d8f6501acf85dfc4430460f01e75ef68
Samuel <37374070+samuelhusein@users.noreply.github.com>
2023-04-15 22:19:29 +0700

    Update

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -1,15 +1,79 @@
-import React, { Component, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSignOut } from "react-auth-kit";
 import { useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+import Pusher from "pusher-js";
+import { useAuthUser } from "react-auth-kit";
+import Cookies from "js-cookie";
+import axios from "axios";
+
 function Header() {
+  const auth = useAuthUser();
+  const userId = auth().userUuid;
+  const [roles, setRoles] = useState([]);
   const signOut = useSignOut();
   const navigate = useNavigate();
+  const [notif, setNotif] = useState([]);
+  const [count, setCount] = useState(0);
   const [activeLink, setActiveLink] = useState("");
   function logout() {
     signOut();
     navigate("/login");
   }
+
+  const fetchData = async () => {
+    const orgId = "71152531-e247-467f-8839-b78c14d7f71e";
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${Cookies.get("_auth")}`,
+        },
+      };
+      console.log(auth().userChange);
+      const response = await axios.get(
+        `${process.env.REACT_APP_BASE_URL}/organization/${userId}/${orgId}/roles`,
+        config
+      );
+      setRoles(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchNotifData = async () => {
+    const orgId = "71152531-e247-467f-8839-b78c14d7f71e";
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${Cookies.get("_auth")}`,
+        },
+      };
+      console.log(auth().userChange);
+      const response = await axios.get(
+        `${process.env.REACT_APP_BASE_URL}/dashboard/notif/${userId}`,
+        config
+      );
+      setCount((response.data ? Object.keys(response.data).length : 0));
+      setNotif(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+    const pusher = new Pusher("7362041596af022ff4f1", {
+      cluster: "ap1",
+      encrypted: true,
+    });
+    console.log(pusher)
+    console.log(roles.organization_role)
+    const channel = pusher.subscribe(`${roles.organization_role}-channel`);
+    channel.bind("broadcast-event", (data) => {
+      console.log(data);
+    });
+    console.log(channel)
+  }, [userId]);
 
   return (
     <div>
@@ -70,18 +134,6 @@ function Header() {
               </a>
             </div>
           </li>
-          {/* <li className="nav-item">
-            <a
-              className="nav-link"
-              data-widget="control-sidebar"
-              onClick={logout}
-              data-controlsidebar-slide="true"
-              href="#"
-              role="button"
-            >
-              <i className="fas fa-sign-out-alt"></i>
-            </a>
-          </li> */}
         </ul>
       </nav>
     </div>
